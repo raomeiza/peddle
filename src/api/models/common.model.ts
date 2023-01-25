944e48e9acd6d3e58371e10051b3de51db49896d
a omeiza <37030673+raomeiza@users.noreply.github.com>
2023-01-25 06:56:59 +0100

    updated the userbasics Schema
    I added address and other relevant but basic fields to the
    userbasics Schema

--- src/api/models/common.model.ts
+++ src/api/models/common.model.ts
@@ -9,6 +9,30 @@ const userBasics:object = new mongoose.Schema({
     type: String,
     max: 50,
   },
+  gender: {
+    type: String,
+    enum: ['male', 'female', 'other']
+  },
+  address: {
+    type: String,
+    max: 150,
+  },
+  country: {
+    type: String,
+    max: 50,
+  },
+  state_province: {
+    type: String,
+    max: 50,
+  },
+  city: {
+    type: String,
+    max: 50,
+  },
+  zip: {
+    type: String,
+    max: 10,
+  },
   email: {
     type: String,
     max: 50,
@@ -25,10 +49,6 @@ const userBasics:object = new mongoose.Schema({
     sparse: true,
     trim: true,
     max: 15,
-  },
-  gender: {
-    type: String,
-    enum: ['male', 'female']
   },
   profile_pic: {
     type: String,

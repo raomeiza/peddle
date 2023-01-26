a273f2478d68e1fc6ad32d0f6f6ae889f398ae14
a omeiza <37030673+raomeiza@users.noreply.github.com>
2023-01-26 02:14:18 +0100

    config files

---
+++ src/config/db.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import logger from '../api/utils/logger'
+import { DATABASE_URL } from '.';
+
+console.log(DATABASE_URL)
+mongoose.set('strictQuery', true);
+const InitiateDB = async () => {
+  try {
+    // @ts-ignore
+    mongoose.connect(DATABASE_URL, {
+      // @ts-ignore
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.log({
+      level: 'info',
+      message: 'Database connection successful',
+    });
+  } catch (ex:any) {
+    logger.log({
+      level: 'error',
+      message: ex.message,
+    });
+    process.exit(1);
+  }
+};
+
+export default InitiateDB;
